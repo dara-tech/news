037b21153ca345d5e94d95d3d48b810d5da23393
Dara <97618988+dara-tech@users.noreply.github.com>
2025-07-16 11:46:44 +0700

    pre-load

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,14 +13,7 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5001/api/:path*',
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const isServer = typeof window === 'undefined';
-
 const api = axios.create({
-  baseURL:
-    isServer
-      ? process.env.INTERNAL_API_URL || 'http://localhost:5001/api' // URL for server-side requests
-      : process.env.NEXT_PUBLIC_API_URL || '/api', // URL for client-side requests
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api',
   withCredentials: true, // Important for sending cookies
 });
 
